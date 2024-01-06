9d2078568cd6133891233ede37dc3511755d1aba
vijaysarma1007 <svmsarma53@gmail.com>
2024-01-06 19:43:27 +0530

    Added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque perspiciatis dicta blanditiis, voluptatibus non dolores quibusdam quam. Dolor repellendus error quaerat
+        nostrum quam aut perspiciatis corrupti odit impedit accusamus. Harum?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt delectus perspiciatis a illo magni quam, laborum provident optio nesciunt totam ratione possimus minima
+        ab iure numquam dolores repellat sunt cum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci accusantium rem numquam placeat dolores fugiat non
+        excepturi, molestias optio alias iure? Vero illum nisi dicta nostrum architecto autem expedita.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, suscipit, laudantium atque ut harum voluptatibus qui saepe sit est doloribus recusandae ab totam ad.
+        Amet tenetur molestias voluptate expedita mollitia?
+      </p>
+    </div>
+  );
+}
